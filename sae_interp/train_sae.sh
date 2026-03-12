2941a5890436a9cb4a3aae4c5313d20e92597586
evag <evag@node3008.inband>
2026-03-12 02:12:40 -0400

    sbatch script

---
+++ sae_interp/train_sae.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=train_sae
+#SBATCH --output=output.log
+#SBATCH --error=error.log
+#SBATCH --time=06:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=4
+
+cd /home/evag/code/physicsnemo/examples/cfd/vortex_shedding_mgn/sae_interp
+source ~/miniconda3/bin/activate
+conda activate nemo311
+
+python train_sae_rand.py
